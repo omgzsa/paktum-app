2f6ba11a97501862be3f7e74cabc436ee3ebc0d9
omgzsa <zselloandras@gmail.com>
2023-05-22 17:25:53 +0200

    green checkbox icon

---
+++ components/icons/IconCheckbox.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineComponent({
+  name: 'IconCheckbox',
+});
+</script>
+
+<template>
+  <svg
+    width="33"
+    height="31"
+    viewBox="0 0 33 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="checkbox">
+      <rect id="bg" x="6" y="6" width="25" height="25" fill="#10D38D" />
+      <g id="tick">
+        <path
+          id="Vector"
+          d="M20.9286 2H2V28.5H28.5V19.6667"
+          stroke="black"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M9.57794 14.6265L15.8875 20.936L31.0303 4.53125"
+          stroke="black"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
