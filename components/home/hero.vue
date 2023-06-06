83b2223d80c947655a1ce0f760ca345f2b5e2a5f
omgzsa <zselloandras@gmail.com>
2023-06-06 15:37:57 +0200

    new link

--- components/home/hero.vue
+++ components/home/hero.vue
@@ -14,16 +14,16 @@ defineProps({
       <div
         class="inline-flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-6 pb-16"
       >
-        <NuxtLink v-slot="{ navigate }" to="/szerzodes/berleti/step-1" custom>
+        <NuxtLink v-slot="{ navigate }" to="/szerzodes/berleti/1" custom>
           <button
             class="px-6 py-3 rounded-lg bg-white font-semibold text-primary-100 drop-shadow hover:bg-accent-300 hover:drop-shadow-md transition-all"
             @click="navigate"
           >
-            Bérlői szerződés
+            Bérleti szerződés
           </button>
         </NuxtLink>
 
-        <NuxtLink v-slot="{ navigate }" to="/szerzodes/berbeado/step-1" custom>
+        <NuxtLink v-slot="{ navigate }" to="/szerzodes/berbeado/1" custom>
           <button
             class="px-6 py-3 rounded-lg bg-primary-100 font-semibold text-white drop-shadow hover:bg-gray-800 hover:drop-shadow-md transition-all"
             @click="navigate"
