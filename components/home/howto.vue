9b08d1040f183dd7f731048f64016fbf2627235f
omgzsa <zselloandras@gmail.com>
2023-06-06 15:38:10 +0200

    spaceing

--- components/home/howto.vue
+++ components/home/howto.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <section class="space-y-8 py-16 site-padding">
+  <section class="space-y-16 py-16 site-padding">
     <h2 class="text-center">{{ title }}</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <HomeHowtoCard
