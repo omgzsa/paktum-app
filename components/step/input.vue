838746946ad31e7a221f07e7e406bc3cf222f4ef
omgzsa <zselloandras@gmail.com>
2023-06-06 15:41:53 +0200

    input for radio buttons

---
+++ components/step/input.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  step: {
+    type: Object,
+    required: true,
+  },
+});
+
+const optionsArr = computed(() => {
+  return props.step.decisions.map((decision) => {
+    return decision.options.map((option) => {
+      return {
+        label: option.label,
+        value: option.isTrue,
+      };
+    });
+  });
+});
+</script>
+
+<template>
+  <div v-for="(point, i) in step.decisions" :key="point.id">
+    <FormKit
+      type="radio"
+      :label="point.question"
+      validation="required"
+      :options="optionsArr[i]"
+    />
+  </div>
+</template>
